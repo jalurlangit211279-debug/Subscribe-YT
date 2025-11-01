e44126d065609076711cb118b5719fe1797bb5d7
jalur langit <jalurlangit211279@gmail.com>
2025-11-01 20:17:33 +0700

    Update dev.php

--- dev.php
+++ dev.php
@@ -3,7 +3,7 @@
  the developer 
  */ 
  // Started // 
-  
+
  // Msg Usage // 
  if ( $argc < 2 ){ 
    print " __     __      _______    _                     
@@ -35,24 +35,18 @@
   |_____/ \__,_|_.__/|___/\___|_|  |_|_.__/ \___| 
   
  "; 
-echo "\033[1;37m[\033[1;32m+\033[1;37m] Powereted By : Jalur Langit."\n";
  echo "\033[1;37m[\033[1;32m+\033[1;37m] Link Channel: \033[1;32m ".$argv[1]."\n"; 
  echo "\033[1;37m[\033[1;32m*\033[1;37m] Tools Used:\033[1;32m Started\n"; 
  echo "\033[1;32m[\033[1;37m~\033[1;32m] Prosses Run: \033[1;33m".date("Y/m/d H:i:s")."\n"; 
  echo "\033[1;32m[\033[1;37m@\033[1;32m] Refresh the Suff[\033[1;37m5\033[1;32m]\033[1;37m Seconds\n\n"; 
  while (1){ 
-$arrContextOptions=array("ssl"=>array("verify_peer"=>false,"verify_peer_name"=>false,),);
-   $channel_id = "ID_SALURAN_ANDA";
- $api_key = "KUNCI_API";
- $api_response = file_get_contents ('https://www.googleapis.com/youtube/v3/channels?part=statistics&id= '. $channel_id . '&fields=items/statistics/subscriberCount&key= '. $api_key );
- $api_response_decoded = json_decode ($api_response,true);
- echo  $api_response_decoded ['items'][0]['statistics']['subscriberCount'];
-   $t = file_get_contents($chanel); 
+   $channel = $argv[1]; 
+   $t = file_get_contents($channel); 
    $pattern = '/yt-uix-tooltip" title="(.*)" tabindex/'; 
-   preg_subscibe($pattern, $t, $subscibe, PREG_OFFSET_CAPTURE); 
-   echo "\033[1;32m[\033[1;37m+\033[1;32m]\033[1;37m Jumlah >>>\033[1;32m ".$chanel-subscribeCount[1]." \033[1;37m<<< \033[1;32mSubscribers\n"; 
+   preg_match($pattern, $t, $matches, PREG_OFFSET_CAPTURE); 
+   echo "\033[1;32m[\033[1;37m+\033[1;32m]\033[1;37m Jumblah >>>\033[1;32m ".$matches[1][0]." \033[1;37m<<< \033[1;32mSubscribers\n"; 
    for($s=5; $s >=0; $s--){ 
-   echo "Sedang Berjalan... [${s}]\r"; 
+   echo "Sedang Berjalan... [ ${s}s ] \r"; 
    sleep(1); 
    } 
    echo ""; 
